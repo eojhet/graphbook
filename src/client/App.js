6ab0858898aef75034e1e340c8e6ad4f743a2179
eojhet <eojhet@gmail.com>
2022-05-11 15:31:42 -0400

    CSS with webpack

--- src/client/App.js
+++ src/client/App.js
@@ -1,8 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+const initialPosts = [
+  {
+    id: 2,
+    text: 'Fart Brains',
+    user: {
+      avatar: '/uploads/avatar1.png',
+      username: 'Turd Ferguson'
+    }
+  },
+  {
+    id: 1,
+    text: 'Yes please!',
+    user: {
+      avatar: '/uploads/avatar2.png',
+      username: 'Spaghetti Man'
+    }
+  }
+]
 
 const App = () => {
+  const [posts, setPosts] = useState(initialPosts);
+
   return (
-    <div>Hello World!</div>
+    <div className='conatiner'>
+      <div className='feed'>
+
+        { posts.map((post, i) => 
+
+          <div key={post.id} className="post">
+            <div className='header'>
+              <img src={post.user.avatar} />
+              <h2>{post.user.username}</h2>
+            </div>
+            <p className='content'>
+              {post.text}
+            </p>
+          </div>
+
+        )}
+
+      </div>
+    </div>
   )
 };
 
