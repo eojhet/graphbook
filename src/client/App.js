4cb10b619c97a267881b79deddc3742a505c5381
Joe Gonzalez <eojhet@gmail.com>
2022-05-11 22:11:55 -0400

    Useful development tools

--- src/client/App.js
+++ src/client/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Helmet } from 'react-helmet';
 import '/assets/css/style.css';
 
 const initialPosts = [
@@ -22,9 +23,41 @@ const initialPosts = [
 
 const App = () => {
   const [posts, setPosts] = useState(initialPosts);
+  const [postContent, setPostContent] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const newPost = {
+      id: posts.length + 1,
+      text: postContent,
+      user: {
+        avatar: '/uploads/avatar1.png',
+        username: 'Fake User'
+      }
+    };
+    setPosts([newPost, ...posts]);
+    setPostContent('');
+  };
 
   return (
     <div className='container'>
+      <Helmet>
+        <title>Graphbook - Feed</title>
+        <meta name="description" content="All your friends are here."></meta>
+      </Helmet>
+      
+      <div className='postForm'>
+        <form onSubmit={handleSubmit}>
+          <textarea 
+            value={postContent} 
+            onChange={(e) => setPostContent(e.target.value)}
+            placeholder="Write your custom post!"
+          />
+          <input type="submit" value="submit" />
+        </form>
+      </div>
+      
+
       <div className='feed'>
 
         { posts.map((post, i) => 
