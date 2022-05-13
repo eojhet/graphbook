a219ce76abaaafeec60bebab1d94a0744b7006d7
eojhet <eojhet@gmail.com>
2022-05-13 13:14:46 -0400

    Chapter 3 Connecting to the Database

--- src/server/index.js
+++ src/server/index.js
@@ -3,10 +3,33 @@ import path from 'path';
 import helmet from 'helmet';
 import cors from 'cors';
 import compress from 'compression';
-
 import services from './services';
 
+const root = path.join(__dirname, '../../');
+
 const app = express();
+app.use(compress());
+
+
+
+
+app.use(helmet());
+app.use(helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ["'self'"],
+    scriptSrc: ["'self'", "'unsafe-inline'"],
+    styleSrc: ["'self'", "'unsafe-inline'"],
+    imgSrc: ["'self'", "data:", "*.amazonaws.com"]
+  }
+}));
+app.use(helmet.referrerPolicy({ policy: 'same-origin' }));
+app.use(cors());
+
+app.use('/', express.static(path.join(root, 'dist/client')));
+app.use('/uploads', express.static(path.join(root, 'uploads')));
+app.get('/', (req, res, next) => {
+  res.sendFile(path.join(root, '/dist/client/index.html'));
+})
 
 // Binding GraphQL server to Express.js web server:
 const serviceNames = Object.keys(services);
@@ -21,28 +44,5 @@ for(let i = 0; i < serviceNames.length; i += 1) {
     app.use('/${name}', services[name]);
   }
 };
-
-app.use(compress());
-app.use(cors());
-
-app.use(helmet());
-app.use(helmet.contentSecurityPolicy({
-  directives: {
-    defaultSrc: ["'self'"],
-    scriptSrc: ["'self'", "'unsafe-inline'"],
-    styleSrc: ["'self'", "'unsafe-inline'"],
-    imgSrc: ["'self'", "data:", "*.amazonaws.com"]
-  }
-}));
-app.use(helmet.referrerPolicy({ policy: 'same-origin' }));
-
-const root = path.join(__dirname, '../../');
-app.use('/', express.static(path.join(root, 'dist/client')));
-app.use('/uploads', express.static(path.join(root, 'public/uploads')));
-
-app.get('/', (req, res, next) => {
-  res.sendFile(path.join(root, '/dist/client/index.html'));
-})
-
 
 app.listen(8000, () => console.log('Listening on port 8000!'));
