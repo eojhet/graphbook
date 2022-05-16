efb6ed821741ac97bb5e08a7a46026382ebf2023
eojhet <eojhet@gmail.com>
2022-05-16 12:40:29 -0400

    One-to-one relationships with Sequelize

--- src/server/services/graphql/resolvers.js
+++ src/server/services/graphql/resolvers.js
@@ -1,42 +1,47 @@
 import logger from '../../helpers/logger.js';
 
-let posts = [
-  {
-    id: 2,
-    text: 'Fart Brains',
-    user: {
-      avatar: '/uploads/avatar1.png',
-      username: 'Turd Ferguson'
-    }
-  },
-  {
-    id: 1,
-    text: 'Yes please!',
-    user: {
-      avatar: '/uploads/avatar2.png',
-      username: 'Spaghetti Man'
-    }
-  }
-]
+// let posts = [
+//   {
+//     id: 2,
+//     text: 'Fart Brains',
+//     user: {
+//       avatar: '/uploads/avatar1.png',
+//       username: 'Turd Ferguson'
+//     }
+//   },
+//   {
+//     id: 1,
+//     text: 'Yes please!',
+//     user: {
+//       avatar: '/uploads/avatar2.png',
+//       username: 'Spaghetti Man'
+//     }
+//   }
+// ]
+export default function resolver() {
 
-const resolvers = {
-  RootQuery: {
-    posts(root, args, context) {
-      return posts;
-    },
-  },
-  RootMutation: {
-    addPost(root, { post, user }, context) {
-      const postObject = {
-        ...post,
-        user,
-        id: posts.length + 1,
-      };
-      posts.push(postObject);
-      logger.log({ level: 'info', message: 'Post was created' });
-      return postObject;
-    },
-  },
-};
+  const { db } = this;
+  const { Post } = db.models;
 
-export default resolvers;
+  const resolvers = {
+    RootQuery: {
+      posts(root, args, context) {
+        return Post.findAll({order: [['createdAt', 'DESC']]});
+      },
+    },
+    RootMutation: {
+      addPost(root, { post, user }, context) {
+        const postObject = {
+          ...post,
+          user,
+          id: posts.length + 1,
+        };
+        posts.push(postObject);
+        logger.log({ level: 'info', message: 'Post was created' });
+        return postObject;
+      },
+    },
+  };
+
+  return resolvers;
+}
