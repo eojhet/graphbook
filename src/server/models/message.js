ed44131e4f82371ae143596c6770a5ff09e77a67
Joe Gonzalez <eojhet@gmail.com>
2022-05-16 21:22:51 -0400

    Chats and messages in GraphQL

--- src/server/models/message.js
+++ src/server/models/message.js
@@ -3,23 +3,24 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
+  class Message extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      this.hasMany(models.Post);
-      this.belongsToMany(models.Chat, { through: 'users_chats' });
+      this.belongsTo(models.User);
+      this.belongsTo(models.Chat);
     }
   }
-  User.init({
-    avatar: DataTypes.STRING,
-    username: DataTypes.STRING
+  Message.init({
+    text: DataTypes.STRING,
+    userId: DataTypes.INTEGER,
+    chatId: DataTypes.INTEGER
   }, {
     sequelize,
-    modelName: 'User',
+    modelName: 'Message',
   });
-  return User;
+  return Message;
 };
