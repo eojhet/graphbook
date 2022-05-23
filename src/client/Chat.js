3e865ec72dca1d2a32ed11146fbceae6c081945a
eojhet <eojhet@gmail.com>
2022-05-23 15:40:20 -0400

    Pagination in React and GraphQL

--- src/client/Chat.js
+++ src/client/Chat.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { gql, useQuery } from '@apollo/client';
+import React, { useState } from 'react';
+import { gql, useQuery, useMutation } from '@apollo/client';
 
 const GET_CHAT = gql`
   query chat($chatId: Int!) {
@@ -21,11 +21,55 @@ const GET_CHAT = gql`
   }
 `;
 
+const ADD_MESSAGE = gql`
+  mutation addMessage($message : MessageInput!) {
+    addMessage(message : $message) {
+      id
+      text
+      user
+      {
+        id
+      }
+    }
+  }
+`;
+
 const Chat = (props) => {
   const { chatId, closeChat } = props;
   const { loading, error, data } = useQuery(GET_CHAT, {
     variables: { chatId }
   });
+  const [text, setText] = useState('');
+  const [addMessage] = useMutation(ADD_MESSAGE, {
+    update(cache, { data: { addMessage } }) {
+      cache.modify({
+        id: cache.identify(data.chat),
+        fields: {
+          messages(existingMessages =[]) {
+            const newMessageRef = cache.writeFragment({
+              data: addMessage,
+              fragment: gql`
+                fragment NewMessage on Chat {
+                  id
+                  type
+                }
+              `
+            });
+            return [...existingMessages, newMessageRef];
+          }
+        }
+      });
+    }
+  });
+
+  const handleKeyPress = (event) => {
+    if (event.key === 'Enter' && text.length) {
+      addMessage({ variables: { message: { text, chatId } } })
+        .then(() => {
+          setText('');
+        })
+    }
+  };
 
   if (loading) return <div className='chatWindow'><p>Loading...</p></div>;
   if (error) return <div className='chatWindow'><p>Loading...</p></div>;
@@ -44,6 +88,9 @@ const Chat = (props) => {
             {message.text}
           </div>
         )}
+      </div>
+      <div className='input'>
+        <input type='text' value={text} onChange={(e) => setText(e.target.value)} onKeyPress={handleKeyPress} />
       </div>
     </div>
   )
